6ec2ab3c863133a07e2a60218e85bf326fcdce85
zzs <1043030462@qq.com>
2021-08-02 11:12:02 +0800

    code

--- yncommon-utils/src/main/java/com/yn/electricity/exception/PasException.java
+++ yncommon-utils/src/main/java/com/yn/electricity/exception/PasException.java
@@ -8,24 +8,38 @@ package com.yn.electricity.exception;
  * @Description:
  */
 public class PasException extends RuntimeException{
+
+    private int code;
+
     public PasException() {
         super();
     }
 
     public PasException(String message) {
         super(message);
+        this.code = code;
     }
 
     public PasException(Throwable cause) {
         super(cause);
+        this.code = code;
     }
 
     public PasException(String message, Throwable cause) {
         super(message, cause);
+        this.code = code;
     }
 
     public PasException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+        this.code = code;
     }
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }
